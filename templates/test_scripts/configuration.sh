fa7ffa3eee8a7365f562601092bef7dbcb6ad8e5
Aakarsh MJ <mj.akarsh@gmail.com>
2024-09-08 10:12:26 +0530

    Updated templates/configuration.sh
    Signed-off-by: Aakarsh MJ <mj.akarsh@gmail.com>

--- templates/test_scripts/configuration.sh
+++ templates/test_scripts/configuration.sh
@@ -1,21 +1,29 @@
 #! /usr/bin/env bash
 
-## This script adds custom shortcuts. Change
-## the default shortcuts locally and push in
-## your MR (together with the usage scenario
-## script).
+## This script adds the option to add user
+## configurations such as shortcuts which might
+## be used to make the automation process easier.
+## Make the changes locally and push in the reqd.
+## configuration file with your MR (together with
+## the usage scenario scripts).
 
+## EXAMPLE: The following is used to load custom shortcuts to okular
 ## SEE: https://invent.kde.org/teams/eco/remote-eco-lab/-/blob/master/scripts/test_scripts/org.kde.okular/configuration.sh
 ## AND: https://invent.kde.org/teams/eco/remote-eco-lab/-/blob/master/scripts/test_scripts/org.kde.okular/part.rc
 
-## Import custom shortcuts
-echo "Importing custom shortcuts"
+## Import user configurations
+echo "Importing user configurations"
 sleep 1
-## Remove previous configuration file:
-## change path to remove any previous shortcuts.
-rm ~/.local/share/kxmlgui5/<PATH/TO/FILENAME>
+
+## Remove previous configuration file(if any present) from KEcoLab:
+## HINT: These maybe present in `~/.local/share` or '~/.config/'
+rm <PATH/TO/CONFIGURATION-FILE>
 sleep 1
-## Change filename and path to import your shortcuts
-cp <FILENAME> ~/.local/share/kxmlgui5/<PATH/TO/FILE>
+
+## Copy the configuration files to the reqd. location.
+## You may be reqd. to create a folder for this.
+[mkdir <PATH/TO/CONFIGURATION-FOLDER]
+cp <FILENAME> <PATH/TO/CONFIGURATION-FILE>
 sleep 1
-echo "Finished importing shortcuts"
+
+echo "Finished importing user configurations"
